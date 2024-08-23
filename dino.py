23082a2cfd53507a32d66b0cadd9d629ba369efc
EtielOguh <Hugodsleite@gmail.com>
2024-08-23 14:29:18 -0300

    Atualização

--- dino.py
+++ dino.py
@@ -2,7 +2,7 @@ import pygame
 from pygame.locals import*
 from sys import exit
 import os
-from random import randrange
+from random import randrange, choice
 
 pygame.init()
 pygame.mixer.init()
@@ -23,6 +23,7 @@ pygame.display.set_caption('Jogo Dino')
 sprite_sheet = pygame.image.load(os.path.join(diretorio_imagens,'dinoSpritesheet.png')).convert_alpha()
 #O convert alpah se a imagem tiver transparencia ele realmente fica transparente, ignora
 
+escolha_obstaculo = choice ([0,1])
 
 class Dino(pygame.sprite.Sprite):
     def __init__(self):
@@ -48,10 +49,10 @@ class Dino(pygame.sprite.Sprite):
         if self.pulo == True:
             if self.rect.y <= 330:
                 self.pulo = False
-            self.rect.y -= 20
+            self.rect.y -= 10
         else:
             if self.rect.y < self.pos_y_inicial:
-                self.rect.y += 20
+                self.rect.y += 10
             else:
                 self.rect.y = self.pos_y_inicial
 
@@ -98,15 +99,44 @@ class Cacto(pygame.sprite.Sprite):
         self.image = pygame.transform.scale(self.image, (32*2, 32*2))
         self.rect = self.image.get_rect() #Pega o retangulo da imagem
         self.mask = pygame.mask.from_surface(self.image)
+        self.escolha = escolha_obstaculo
         self.rect.center = (LARGURA, ALTURA - 64)#x, y
+        self.rect.x = LARGURA
 
     def update(self):
-        if self.rect.topright[0] < 0:
-            self.rect.x = LARGURA
-        self.rect.x -= 10
-        
+        if self.escolha == 0:
+            if self.rect.topright[0] < 0:
+                self.rect.x = LARGURA
+            self.rect.x -= 10
 
+class DinoVoador(pygame.sprite.Sprite):
+    def __init__(self):
+        pygame.sprite.Sprite.__init__(self)
+        self.imagens_dinossauro = []
+        for i in range(3,5):
+            img = sprite_sheet.subsurface((i*32, 0),(32, 32))
+            img = pygame.transform.scale(img, (32*3, 32*3))
+            self.imagens_dinossauro.append(img)
 
+        self.index_lista = 0
+        self.image = self.imagens_dinossauro[self.index_lista]
+        self.mask = pygame.mask.from_surface(self.image)
+        self.escolha = escolha_obstaculo
+        self.rect = self.image.get_rect()
+        self.rect.center = (LARGURA, 300) #Colocar na tela
+        self.rect.x = LARGURA
+    
+    def update(self):
+        if self.escolha == 1:
+            if self.rect.topright[0] < 0:
+                self.rect.x = LARGURA
+            self.rect.x -= 10
+
+            if self.index_lista > 1:
+                self.index_lista = 0
+            self.index_lista += 0.15
+            self.image = self.imagens_dinossauro[int(self.index_lista)]
+    
 todas_as_sprites = pygame.sprite.Group()
 dino = Dino()
 todas_as_sprites.add(dino)
@@ -122,13 +152,17 @@ for i in range(640*2//64):
 cacto = Cacto()
 todas_as_sprites.add(cacto)
 
+dino_voador = DinoVoador()
+todas_as_sprites.add(dino_voador)
+
 grupo_obstaculos = pygame.sprite.Group()
 grupo_obstaculos.add(cacto)
+grupo_obstaculos.add(dino_voador)
 
 relogio = pygame.time.Clock()
 
 while True:
-    relogio.tick(30)
+    relogio.tick(25)
     tela.fill(BRANCO)
     for event in pygame.event.get():
         if event.type == QUIT:
@@ -144,6 +178,14 @@ while True:
     colisoes = pygame.sprite.spritecollide(dino, grupo_obstaculos, False, pygame.sprite.collide_mask) #Analisar
     
     todas_as_sprites.draw(tela)
+
+    if cacto.rect.topright[0] <= 0 or dino_voador.rect.topright[0] <= 0:
+        escolha_obstaculo = choice([0,1])
+        cacto.rect.x = LARGURA
+        dino_voador.rect.x = LARGURA
+        cacto.escolha = escolha_obstaculo
+        dino_voador.escolha = escolha_obstaculo
+
     if colisoes:
         pass
     else:
