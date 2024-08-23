fb165e6cd26ea1dea2d7295d668b9f3bd2502527
EtielOguh <Hugodsleite@gmail.com>
2024-08-23 10:44:26 -0300

    Jogo

---
+++ dino.py
@@ -0,0 +1,152 @@
+import pygame
+from pygame.locals import*
+from sys import exit
+import os
+from random import randrange
+
+pygame.init()
+pygame.mixer.init()
+
+diretorio_principal = os.path.dirname(__file__)
+diretorio_imagens = os.path.join(diretorio_principal,'imagens')
+diretorio_sons = os.path.join(diretorio_principal, 'sons')
+
+LARGURA = 640
+ALTURA = 480
+
+BRANCO = (255,255,255)
+
+tela = pygame.display.set_mode((LARGURA, ALTURA))
+
+pygame.display.set_caption('Jogo Dino')
+
+sprite_sheet = pygame.image.load(os.path.join(diretorio_imagens,'dinoSpritesheet.png')).convert_alpha()
+#O convert alpah se a imagem tiver transparencia ele realmente fica transparente, ignora
+
+
+class Dino(pygame.sprite.Sprite):
+    def __init__(self):
+        pygame.sprite.Sprite.__init__(self)
+        self.imagens_dinossauro = []
+        for i in range(3):
+            img = sprite_sheet.subsurface((i * 32,0), (32,32))
+            img= pygame.transform.scale(img,(32*2, 32*2))
+            self.imagens_dinossauro.append(img)
+        
+        self.indexlista = 0
+        self.image = self.imagens_dinossauro[self.indexlista]
+        self.rect = self.image.get_rect()
+        self.mask = pygame.mask.from_surface(self.image) #Criando a mascara da Sprite
+        self.pos_y_inicial = ALTURA - 56 - 64//2
+        self.rect.center = (100,ALTURA-56)
+        self.pulo = False
+    
+    def pular(self):
+        self.pulo = True
+
+    def update(self):
+        if self.pulo == True:
+            if self.rect.y <= 330:
+                self.pulo = False
+            self.rect.y -= 20
+        else:
+            if self.rect.y < self.pos_y_inicial:
+                self.rect.y += 20
+            else:
+                self.rect.y = self.pos_y_inicial
+
+
+        if self.indexlista > 2:
+            self.indexlista = 0
+        self.indexlista += 0.25
+        self.image = self.imagens_dinossauro[int(self.indexlista)]
+
+class Nuvens(pygame.sprite.Sprite):
+    def __init__(self):
+        pygame.sprite.Sprite.__init__(self)
+        self.image = sprite_sheet.subsurface((7*32, 0), (32,32))
+        self.image = pygame.transform.scale(self.image, (32*3, 32*3))
+        self.rect = self.image.get_rect()
+        self.rect.y = randrange(0,150, 50) #um numero entre 50 e 200 pulando de x em x
+        self.rect.x = LARGURA - randrange(30, 300, 90)
+        #self.rect.center = (100,100)
+    
+    def update(self):
+        if self.rect.topright[0] < 0:
+            self.rect.y = randrange(0,150, 50)
+            self.rect.x = LARGURA
+        self.rect.x -= 8
+
+class Chao(pygame.sprite.Sprite):
+    def __init__(self, pos_x):
+        pygame.sprite.Sprite.__init__(self)
+        self.image = sprite_sheet.subsurface((6*32, 0), (32,32))
+        self.image = pygame.transform.scale(self.image, (32*2, 32*2))
+        self.rect = self.image.get_rect()
+        self.rect.y = ALTURA - 64
+        self.rect.x = pos_x * 64
+    
+    def update(self):
+        if self.rect.topright[0] < 0:
+            self.rect.x = LARGURA
+        self.rect.x -= 8
+
+class Cacto(pygame.sprite.Sprite):
+    def __init__(self):
+        pygame.sprite.Sprite.__init__(self)
+        self.image = sprite_sheet.subsurface((5*32, 0), (32,32))
+        self.image = pygame.transform.scale(self.image, (32*2, 32*2))
+        self.rect = self.image.get_rect() #Pega o retangulo da imagem
+        self.mask = pygame.mask.from_surface(self.image)
+        self.rect.center = (LARGURA, ALTURA - 64)#x, y
+
+    def update(self):
+        if self.rect.topright[0] < 0:
+            self.rect.x = LARGURA
+        self.rect.x -= 10
+        
+
+
+todas_as_sprites = pygame.sprite.Group()
+dino = Dino()
+todas_as_sprites.add(dino)
+
+for i in range(4):
+    nuvem = Nuvens()
+    todas_as_sprites.add(nuvem)
+
+for i in range(640*2//64):
+    chao = Chao(i)
+    todas_as_sprites.add(chao)
+
+cacto = Cacto()
+todas_as_sprites.add(cacto)
+
+grupo_obstaculos = pygame.sprite.Group()
+grupo_obstaculos.add(cacto)
+
+relogio = pygame.time.Clock()
+
+while True:
+    relogio.tick(30)
+    tela.fill(BRANCO)
+    for event in pygame.event.get():
+        if event.type == QUIT:
+            pygame.quit()
+            exit()
+        if event.type == KEYDOWN:
+            if event.key == K_SPACE:
+                if dino.rect.y != dino.pos_y_inicial:
+                    pass
+                else:
+                    dino.pular()
+    
+    colisoes = pygame.sprite.spritecollide(dino, grupo_obstaculos, False, pygame.sprite.collide_mask) #Analisar
+    
+    todas_as_sprites.draw(tela)
+    if colisoes:
+        pass
+    else:
+        todas_as_sprites.update()
+
+    pygame.display.flip()
