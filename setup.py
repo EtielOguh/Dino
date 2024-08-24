66276a2679b0b4b2cdb186a482d6450466b18937
EtielOguh <Hugodsleite@gmail.com>
2024-08-24 10:42:35 -0300

    Setup TESTE

--- setup.py
+++ setup.py
@@ -1,9 +1,14 @@
 import cx_Freeze
 
-executables = [cx_Freeze.Executable('dino.py')]
+executables = [cx_Freeze.Executable("dino.py")]
 
 cx_Freeze.setup(
-    name="dino game",
-    options ={'build_exe': {'packages': ['pygame']},'include_files':['imagens', 'sons']},
-    executables = executables
+    name="Dino",
+    options={
+        "build_exe": {
+            "packages": ["pygame"],
+            "include_files": ["imagens", "Sons"]
+        }
+    },
+    executables=executables
 )
